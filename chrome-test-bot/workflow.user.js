16e2dab236c3b2adb6ac75a20bd3b040e0cdfef4
Behram ÇELEN <behramcelen@gmail.com>
2016-07-29 06:23:40 +0300

    start test from login as author and test until submit article fully

--- chrome-test-bot/workflow.user.js
+++ chrome-test-bot/workflow.user.js
@@ -1,21 +1,6 @@
 var domain = document.domain;
 var url = document.URL;
 
-function getSearchParameters() {
-      var prmstr = window.location.search.substr(1);
-      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
-}
-
-function transformToAssocArray( prmstr ) {
-    var params = {};
-    var prmarr = prmstr.split("&");
-    for ( var i = 0; i < prmarr.length; i++) {
-        var tmparr = prmarr[i].split("=");
-        params[tmparr[0]] = tmparr[1];
-    }
-    return params;
-}
-
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -38,7 +23,105 @@ function getCookie(cname) {
     return "";
 }
 
+function startArticleSubmission(){
+
+    setCookie('articleSubmissionStarted', 1, 24);
+    loginAsAuthor();
+}
+
+function loginAsAuthor(){
+    var current_user = $('.user-menu .dropdown-menu li:eq(0) a').html();
+    console.log('current user: '+ current_user);
+    if(current_user == '@demo_author'){
+        console.log('Correct author user.');
+    }else{
+        window.location = "http://ojs.dev/logout";
+    }
+}
+
+function clickArticleSubmitButton(){
+    var subformCount = $('#parse-citations-button').parent().parent().parent().find('.submission-subform').length;
+    if(subformCount < 1){
+        console.log('not this time');
+        return null;
+    }
+    console.log(typeof submittedArticleForm);
+    if(typeof submittedArticleForm == 'boolean'){
+        return;
+    }
+    submittedArticleForm = true;
+    $('#ojs_article_submission_save').click();
+    console.log('i clicked to submit form');
+    setCookie('submitArticleOnPreview', 1, 8);
+}
+
+function fillArticleTrAbstract(){
+
+    if(typeof abstractTransformed !== 'undefined'){
+        return;
+    }
+    abstractTransformed = true;
+    $('#ojs_article_submission_translations_tr_abstract').remove();
+    $('form[name="ojs_article_submission"]').append('<input name="ojs_article_submission[translations][tr][abstract]" value="hello baby whats up" />');
+}
+
 if(url == "http://ojs.dev/#workflow-test"){
 
-    //dosomething();
+    startArticleSubmission();
+}
+
+if(url == "http://ojs.dev/"){
+    if(getCookie('articleSubmissionStarted') == '1'){
+        window.location = "http://ojs.dev/login";
+    }
+}
+
+if(url == "http://ojs.dev/login"){
+    if(getCookie('articleSubmissionStarted') == '1'){
+        $("#username").val('demo_author');
+        $("#password").val('demo');
+        $("#password").parent().parent().find('button').click();
+    }
+}
+
+if(url == 'http://ojs.dev/user'){
+    if(getCookie('articleSubmissionStarted') == '1'){
+        window.location = 'http://ojs.dev/journal/2/submission/start#workflow-test';
+    }
+}
+
+if(url == 'http://ojs.dev/journal/2/submission/start#workflow-test'){
+    $('#ojs_article_submission_checks_0').prop( "checked", true );
+    $('#ojs_article_submission_checks_1').prop( "checked", true );
+    $('#ojs_article_submission_checks_2').prop( "checked", true );
+    $('#ojs_article_submission_save').click();
+}
+
+if(url == 'http://ojs.dev/journal/2/submission/new#workflow-test'){
+    $('#ojs_article_submission_articleType').val(5);
+    $('#ojs_article_submission_subjects').val([3,4]);
+    $('#ojs_article_submission_translations_tr_title').val('hello workflow test tr title');
+    $('#ojs_article_submission_translations_tr_keywords').append('<option selected>behram</option>\
+    <option selected>workflow</option>\
+    <option selected>test</option>');
+    setInterval(function(){ fillArticleTrAbstract(); }, 1000);
+    $('#raw-citations').val('Amann, Markus. Pulmonary system limitations to endurance exercise performance in humans. Exp Physiol, 2012; 97(3):311–18.');
+    $('#parse-citations-button').click();
+    $('#ojs_article_submission_articleFiles_0_file').val('article.txt');
+    $('#ojs_article_submission_articleFiles_0_title').val('sample workflow test file title ');
+    $('#ojs_article_submission_articleFiles_0_description').val('sample workflow test file description');
+
+    setInterval(function(){ clickArticleSubmitButton(); }, 1000);
+}
+
+if(url.match(/journal\/2\/submission\/preview\//g)){
+    if(getCookie('submitArticleOnPreview') == '1'){
+        $('#ojs_article_submission_note').val('Merhaba editör bu ilk makalem lütfen kkabul edin!');
+        var myJavaScript = "var confirm = false;";
+        var scriptTag = document.createElement("script");
+        scriptTag.innerHTML = myJavaScript;
+        document.head.appendChild(scriptTag);
+
+        $('#ojs_article_submission_submit').click();
+    }
 }
