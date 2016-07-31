08d6127881c05f7c093f50541d887a8f97b10638
Behram ÇELEN <behramcelen@gmail.com>
2016-07-31 07:44:12 +0300

    create article workflow step entity and related orm file

---
+++ Entity/ArticleWorkflowStep.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Dergipark\WorkflowBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Ojs\UserBundle\Entity\User;
+
+/**
+ * Class ArticleWorkflowStep
+ * @package Dergipark\WorkflowBundle\Entity
+ */
+class ArticleWorkflowStep
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var integer
+     */
+    private $order;
+
+    /**
+     * @var ArrayCollection|User[]
+     */
+    public $grantedUsers;
+
+    /**
+     * @var ArticleWorkflow
+     */
+    protected $articleWorkflow;
+
+    /**
+     * Step constructor.
+     *
+     */
+    public function __construct()
+    {
+        $this->grantedUsers = new ArrayCollection();
+    }
+
+    /**
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param  integer $order
+     * @return $this
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return ArrayCollection|User[]
+     */
+    public function getGrantedUsers()
+    {
+        return $this->grantedUsers;
+    }
+
+    /**
+     * @param  User $user
+     * @return $this
+     */
+    public function addGrantedUser(User $user)
+    {
+        if (!$this->grantedUsers->contains($user)) {
+            $this->grantedUsers->add($user);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param  User $user
+     * @return $this
+     */
+    public function removeGrantedUser(User $user)
+    {
+        if ($this->grantedUsers->contains($user)) {
+            $this->grantedUsers->removeElement($user);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function hasGrantedUser(User $user)
+    {
+        if ($this->grantedUsers->contains($user)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return ArticleWorkflow
+     */
+    public function getArticleWorkflow()
+    {
+        return $this->articleWorkflow;
+    }
+
+    /**
+     * @param ArticleWorkflow $articleWorkflow
+     *
+     * @return $this
+     */
+    public function setArticleWorkflow($articleWorkflow)
+    {
+        $this->articleWorkflow = $articleWorkflow;
+
+        return $this;
+    }
+}
