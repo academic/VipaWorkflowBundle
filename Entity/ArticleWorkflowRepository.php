4a672954716fa24b9866c316b4303aa5b29b96d2
Behram ÇELEN <behramcelen@gmail.com>
2016-08-01 00:11:38 +0300

    create repository of entity class which needs

---
+++ Entity/ArticleWorkflowRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dergipark\WorkflowBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+
+/**
+ * Class ArticleWorkflowRepository
+ * @package Dergipark\WorkflowBundle\Entity
+ */
+class ArticleWorkflowRepository extends EntityRepository
+{
+}
---
+++ Entity/ArticleWorkflowStepRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dergipark\WorkflowBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+
+/**
+ * Class ArticleWorkflowStepRepository
+ * @package Dergipark\WorkflowBundle\Entity
+ */
+class ArticleWorkflowStepRepository extends EntityRepository
+{
+}
