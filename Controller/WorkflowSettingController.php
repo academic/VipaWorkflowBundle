c13616dddf49a9165df98e5fbbf5cc01d7479a05
Behram ÇELEN <behramcelen@gmail.com>
2016-07-31 06:50:46 +0300

    journal wf step granted users setup logic support

--- Controller/WorkflowSettingController.php
+++ Controller/WorkflowSettingController.php
@@ -3,9 +3,10 @@
 namespace Dergipark\WorkflowBundle\Controller;
 
 use Dergipark\WorkflowBundle\Entity\JournalWorkflowSetting;
+use Dergipark\WorkflowBundle\Entity\JournalWorkflowStep;
 use Dergipark\WorkflowBundle\Form\Type\JournalWfSettingType;
+use Dergipark\WorkflowBundle\Form\Type\JournalWfStepType;
 use Ojs\CoreBundle\Controller\OjsController as Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,7 +17,10 @@ class WorkflowSettingController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('DergiparkWorkflowBundle:WorkflowSetting:_workflow_setting.html.twig');
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        return $this->render('DergiparkWorkflowBundle:WorkflowSetting:_workflow_setting.html.twig',[
+            'journal' => $journal,
+        ]);
     }
 
     /**
@@ -28,7 +32,6 @@ class WorkflowSettingController extends Controller
         $journal = $this->get('ojs.journal_service')->getSelectedJournal();
         $this->throw404IfNotFound($journal);
         $em = $this->getDoctrine()->getManager();
-        $translator = $this->get('translator');
 
         $journalWorkflowSetting = $em->getRepository(JournalWorkflowSetting::class)->findOneBy([
             'journal' => $journal,
@@ -51,6 +54,45 @@ class WorkflowSettingController extends Controller
         }
 
         return $this->render('DergiparkWorkflowBundle:WorkflowSetting:_basic_journal_wf_setting.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function stepUsersSetupAction(Request $request, $stepOrder)
+    {
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        $this->throw404IfNotFound($journal);
+        $em = $this->getDoctrine()->getManager();
+
+        $journalWorkflowStep = $em->getRepository(JournalWorkflowStep::class)->findOneBy([
+            'journal' => $journal,
+            'order' => $stepOrder,
+        ]);
+        if(!$journalWorkflowStep){
+            $journalWorkflowStep = new JournalWorkflowStep();
+        }
+        $journalWorkflowStep->setJournal($journal);
+        $journalWorkflowStep->setOrder($stepOrder);
+
+        $form = $this->createForm(new JournalWfStepType(), $journalWorkflowStep, [
+            'action' => $this->generateUrl('dergipark_workflow_step_users_setup', [
+                'journalId' => $journal->getId(),
+                'stepOrder' => $stepOrder,
+            ])
+        ]);
+        $form->handleRequest($request);
+
+        if($request->getMethod() == 'POST' && $form->isValid()){
+            $em->persist($journalWorkflowStep);
+            $em->flush();
+            $this->successFlashBag('successful.update');
+        }
+
+        return $this->render('DergiparkWorkflowBundle:WorkflowSetting:_workflow_step_setting.html.twig', [
             'form' => $form->createView(),
         ]);
     }
--- Resources/public/js/workflow.js
+++ Resources/public/js/workflow.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $('.fancybox').fancybox({});
     OjsWorkflow = {
         basicJournalWfSetting: function($this) {
             $this = $($this);
@@ -7,16 +8,39 @@ $(document).ready(function() {
                 href: $this.attr('href')
             });
         },
-        stepGrantedUsersSetup: function($this, $stepOrder) {
-            $.fancybox( '<h1>Lorem lipsum</h1>' );
-        },
-        showStepDetail: function($this, $stepOrder) {
-            $.fancybox(Translator.trans('Confirm'));
-        },
         updateJournalWfSetting: function () {
             var journalWfSettingForm = $('form[name="journal_wf_setting"]');
             $.post( journalWfSettingForm.attr('action'), journalWfSettingForm.serialize(), function( data ) {
                 $.fancybox(data);
+            });
+        },
+        stepGrantedUsersSetup: function($this, $stepOrder) {
+            $.fancybox({
+                type: 'ajax',
+                href: Routing.generate('dergipark_workflow_step_users_setup', {journalId: journalId, stepOrder: $stepOrder}),
+                autoSize: false,
+                width: '600px',
+                maxWidth: '600px',
+                height: 'auto'
+            });
+        },
+        updateGrantedUsers: function() {
+            var journalWfStepForm = $('form[name="journal_wf_step"]');
+            $.post( journalWfStepForm.attr('action'), journalWfStepForm.serialize(), function( data ) {
+                $.fancybox({
+                    content: data,
+                    type: 'inline',
+                    autoSize: false,
+                    width: '600px',
+                    maxWidth: '600px',
+                    height: 'auto'
+                });
+            });
+        },
+        showStepDetail: function($this, $stepOrder) {
+            $.fancybox({
+                type: 'inline',
+                href: '#wf-step-description-'+$stepOrder
             });
         }
     };
