f2fe2123428cf1a4a6ab581bb036550d82cea1b4
Behram ÇELEN <behramcelen@gmail.com>
2016-08-05 07:45:43 +0300

    create review form entity and orm files

---
+++ Entity/JournalReviewFormRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dergipark\WorkflowBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+
+/**
+ * Class JournalReviewFormRepository
+ * @package Dergipark\WorkflowBundle\Entity
+ */
+class JournalReviewFormRepository extends EntityRepository
+{
+}
