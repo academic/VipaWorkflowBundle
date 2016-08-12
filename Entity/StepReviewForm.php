e842ca65ffc8dc8d1007049fb23befdf6e869676
Behram ÇELEN <behramcelen@gmail.com>
2016-08-12 15:25:10 +0300

    create step review form entity and related orm files

---
+++ Entity/StepReviewForm.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Dergipark\WorkflowBundle\Entity;
+
+/**
+ * Class JournalReviewForm
+ * @package Dergipark\WorkflowBundle\Entity
+ */
+class StepReviewForm
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var bool
+     */
+    private $active = true;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var ArticleWorkflowStep
+     */
+    private $step;
+
+    /**
+     * DialogPost constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param boolean $active
+     *
+     * @return $this
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return $this
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return JournalWorkflowStep
+     */
+    public function getStep()
+    {
+        return $this->step;
+    }
+
+    /**
+     * @param JournalWorkflowStep $step
+     *
+     * @return $this
+     */
+    public function setStep($step)
+    {
+        $this->step = $step;
+
+        return $this;
+    }
+}
